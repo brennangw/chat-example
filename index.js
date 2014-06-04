d84aee1200772f6648eb25dd7ad245965b339e77
brennangw <brennangoffwallace@gmail.com>
2014-06-03 19:21:05 -0500

    working on multiple chat rooms

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ var http = require('http').Server(app);  //getting an https erver onject that wi
 var io = require('socket.io')(http); // creating a io object. I asked about the http server part. 
 
 var rooms = [];
-function Room(){
+function Room(name){
+  this.name = name;
   this.clients = [];
   this.password;
 }
-//seeing if this works
-rooms[0] = new Room();
 
 app.get('/', function(req, res){ // get request at the root the call back's
   res.sendfile('index.html');    // response object (res) servers the index.html file
@@ -22,10 +21,25 @@ app.get('/chat', function(req, res){ // get request at the root the call back's
 
 io.on('connection', function(socket){
 
-	socket.on('join', function(name){
-		socket.nickname = name;
-		io.emit('chat message', 'WELCOME: ' + name);
-	});
+  socket.on('requestEntry', function(strInfo){
+    var info = strInfo.split(",");
+    socket.nickname = String(info[0]);
+  });
+
+  socket.on('startRoom', function(strInfo){
+    var info = strInfo.split(",");
+    var newRoom = new Room(info[1]);
+    socket.nickname = String(info[0]);
+    newRoom.clients.push(socket);
+    rooms.push(newRoom);
+    var index = rooms.indexOf(newRoom);
+    io.emit('giveRoomIndex', index);
+  });
+
+	//socket.on('join', function(name){
+	//	socket.nickname = name;
+	//	io.emit('chat message', 'WELCOME: ' + name);
+	//});
 
   	socket.on('chat message', function(msg){
   		socket.broadcast.emit('chat message', socket.nickname + ": " + msg);
