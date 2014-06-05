b5b75b10e7ef3ed0e84a767f4a6dfba2ab51c357
brennangw <brennangoffwallace@gmail.com>
2014-06-05 01:10:59 -0500

    added more server side checks
    this includes: passwords & unique client names

--- index.js
+++ index.js
@@ -22,25 +22,50 @@ app.get('/chat', function(req, res){ // get request at the root the call back's
 io.on('connection', function(socket){
 
   socket.on('requestEntry', function(strInfo){
-    console.log("passed string: " + strInfo);
+    
     var info = strInfo.split(",");
     socket.nickname = String(info[0]);
+    console.log("passed string: " + strInfo);
     console.log("trying to find room: " + info[1]);
 
-    var index;
-    for (count = 0; count < rooms.length; ++count){
+      var index;
+      for (count = 0; count < rooms.length; ++count){ //entry checks, check the room name, password, unique
 
-      console.log("name: " + rooms[count].name);
+        console.log("name: " + rooms[count].name);
 
-        if (String(rooms[count].name) === String(info[1])) {
-          rooms[count].clients.push(socket);
-          console.log("found the room");
-          index = count;
-        }
+          if (String(rooms[count].name) === String(info[1])) { //room name check
+            console.log("passed room name check");
+            
+            console.log("password: " + String(rooms[count].password));
+            if (String(rooms[count].password) === String(info[2])){ //password check
+              console.log("passed room password check");
+
+              var uniqueName = true;
+
+              for (var j = 0; j < rooms[count].clients.length; j++) { //unique name check
+                if (String(rooms[count].clients[j]) === String(info[0])) {
+                  uniqueName = false;
+                }
+              }
+
+              if (uniqueName) { 
+                rooms[count].clients.push(socket);
+                console.log("found the room and socket added to it");
+                index = count;
+                socket.emit('giveRoomIndex', index);
+              } else { //FAILING UNIQUE NAME TEST
+                socket.emit('giveRoomIndex', "non-unique name");
+              }
+
+            } else { //FAILNG PASSWORD TEST
+              console.log("failed password");
+              socket.emit('giveRoomIndex', "failed password");
+            }
+      }
+      console.log("emiting giveRoomIndex next.");
     }
-    console.log("emiting giveRoomIndex next.");
-    socket.emit('giveRoomIndex', index);
   });
+
   
   socket.on('startRoom', function(strInfo){
     console.log("passed string: " + strInfo);
@@ -48,6 +73,7 @@ io.on('connection', function(socket){
     var newRoom = new Room(info[1]);
     socket.nickname = String(info[0]);
     console.log("started room: " + newRoom.name)
+    newRoom.password = String(info[2]);
     newRoom.clients.push(socket);
     rooms.push(newRoom);
     var index = rooms.indexOf(newRoom);
@@ -82,8 +108,3 @@ var port = 3000;
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
-
-
-
-
-
